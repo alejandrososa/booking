0a53c07ad40ee8d365b838e491d04529eea353c3
Alex Sosa <elpastorsalomon@gmail.com>
2019-10-17 22:09:03 +0200

    Add symfony ddd base

--- public/index.php
+++ public/index.php
@@ -1,8 +1,27 @@
 <?php
-/**
- * Booking, Created by PhpStorm.
- * @author: Alejandro Sosa <alesjohnson@hotmail.com>
- * @copyright Copyright (c) 2019, 19/02/2019 11:55
- */
 
-echo "Hello Booking";
+use Booking\Kernel;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/config/bootstrap.php';
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
